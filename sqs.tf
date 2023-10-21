93da06dbf46cfe5f7ae5ba59b9fd7ba3bb8845c0
Thiago Panini <panini.development@gmail.com>
2023-10-20 22:31:31 -0300

    feat: iniciando construções da fila SQS

--- sqs.tf
+++ sqs.tf
@@ -10,3 +10,17 @@ do projeto.
     SQS queue
     pynvest-tickers-queue
 ------------------------------------------------------- */
+
+# Definindo fila SQS para recebimento de tickers
+resource "aws_sqs_queue" "pynvest-tickers-queue" {
+  name = var.sqs_tickers_queue_name
+
+  visibility_timeout_seconds = var.sqs_visibility_timeout_seconds
+  message_retention_seconds  = var.sqs_message_retention_seconds
+  max_message_size           = var.sqs_max_message_size
+  delay_seconds              = var.sqs_delay_seconds
+  receive_wait_time_seconds  = var.sqs_receive_wait_time_seconds
+
+  sqs_managed_sse_enabled = true
+
+}
--- variables.tf
+++ variables.tf
@@ -5,3 +5,51 @@ Arquivo contendo todas as variáveis do módulo Terraform
 definidas neste projeto.
 -------------------------------------------------------- */
 
+/* -------------------------------------------------------
+    VARIABLES: SQS
+    Variáveis de definição de filas SQS do módulo
+------------------------------------------------------- */
+
+/*
+ToDos:
+    - Configurar policy para acesso básico à fila SQS (put message e get queue URL com resources restritos (começando com pynvest?))
+    - Adicionar policy à role da Lambda de get tickers (remover a s3 policy)
+    - Testar
+    - Configurar access policy da fila em si para restringir ainda mais as permissões de put e get messages
+*/
+
+variable "sqs_tickers_queue_name" {
+  description = "Nome da fila SQS responsável por receber as mensagens contendo informações dos tickers extraídos"
+  type        = string
+  default     = "pynvest-tickers-queue"
+}
+
+variable "sqs_visibility_timeout_seconds" {
+  description = "Tempo (em segundos) em que uma mensagem recebida por um consumidor ficará invisível para outros consumidores"
+  type        = number
+  default     = 60
+}
+
+variable "sqs_message_retention_seconds" {
+  description = "Tempo (em segundos) em que uma mensagem não deletada continua armazenada. Após esse período, as mensagens da fila são deletadas"
+  type        = number
+  default     = 3600
+}
+
+variable "sqs_max_message_size" {
+  description = "Tamanho máximo (em bytes) das mensagens da fila"
+  type        = number
+  default     = 131072 # 120Kb
+}
+
+variable "sqs_delay_seconds" {
+  description = "Delay em que novas mensagens chegam à fila caso os consumidores necessitem de mais tempo para processamento"
+  type        = number
+  default     = 0
+}
+
+variable "sqs_receive_wait_time_seconds" {
+  description = "Tempo máximo (em segundos) que processos de pooling irão aguardar por mensagens disponíveis (Short Pooling versus Long Pooling)"
+  type        = number
+  default     = 0
+}
