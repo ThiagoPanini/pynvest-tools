79bab040d045c66aad10b4c4b63f1cd76dd0ee58
Thiago Panini <panini.development@gmail.com>
2023-10-22 21:50:58 -0300

    feat: entregando lambda para envio de mensagens para fila SQS

--- lambda.tf
+++ lambda.tf
@@ -8,27 +8,28 @@ projeto.
 
 /* -------------------------------------------------------
     Lambda function
-    pynvest-get-tickers-to-queue
+    pynvest-lambda-send-b3-tickers-to-sqs-queue
 ------------------------------------------------------- */
 
 # Criando pacote zip da função a ser criada
-data "archive_file" "pynvest-get-tickers-to-queue" {
+data "archive_file" "pynvest-lambda-send-b3-tickers-to-sqs-queue" {
   type        = "zip"
-  source_dir  = "${path.module}/app/lambda/functions/pynvest-get-tickers-to-queue/"
-  output_path = "${path.module}/app/lambda/zip/pynvest-get-tickers-to-queue.zip"
+  source_dir  = "${path.module}/app/lambda/functions/pynvest-lambda-send-b3-tickers-to-sqs-queue/"
+  output_path = "${path.module}/app/lambda/zip/pynvest-lambda-send-b3-tickers-to-sqs-queue.zip"
 }
 
 # Criando função Lambda
-resource "aws_lambda_function" "pynvest-get-tickers-to-queue" {
-  function_name = "pynvest-get-tickers-to-queue"
-  filename      = "${path.module}/app/lambda/zip/pynvest-get-tickers-to-queue.zip"
-  role          = aws_iam_role.pynvest-lambda-get-raw-data-role.arn
+resource "aws_lambda_function" "pynvest-lambda-send-b3-tickers-to-sqs-queue" {
+  function_name = "pynvest-lambda-send-b3-tickers-to-sqs-queue"
+  description   = "Coleta tickers de ativos da B3 e envia mensagens para fila SQS"
 
-  source_code_hash = data.archive_file.pynvest-get-tickers-to-queue.output_base64sha256
+  filename         = "${path.module}/app/lambda/zip/pynvest-lambda-send-b3-tickers-to-sqs-queue.zip"
+  source_code_hash = data.archive_file.pynvest-lambda-send-b3-tickers-to-sqs-queue.output_base64sha256
 
+  role    = aws_iam_role.pynvest-lambda-send-b3-tickers-to-sqs-queue.arn
   handler = "lambda_function.lambda_handler"
   runtime = "python3.10"
-  timeout = 30
+  timeout = 180
 
   layers = [
     "arn:aws:lambda:${local.region_name}:336392948345:layer:AWSSDKPandas-Python310:5"
