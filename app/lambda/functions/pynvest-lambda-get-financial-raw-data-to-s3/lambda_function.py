c58bfa1a031d6b3280e81c0d6a9e8a0f632c796f
Thiago Panini <panini.development@gmail.com>
2023-10-22 22:22:11 -0300

    feat: mvp da lambda para processamento de msgs da fila

---
+++ app/lambda/functions/pynvest-lambda-get-financial-raw-data-to-s3/lambda_function.py
@@ -0,0 +1,49 @@
+# Importando bibliotecas
+import boto3
+import json
+
+from pynvest.scrappers.fundamentus import Fundamentus
+from pynvest.utils.log import log_config
+import logging
+
+
+# Configurando objeto logger
+logger = log_config(logger_name=__file__, logger_level=logging.INFO)
+logger.propagate = False
+
+
+# Definindo função handler
+def lambda_handler(
+    event,
+    context,
+    sqs_queue_name: str = "pynvest-tickers-queue"
+):
+    """
+    Extração de tickers de Ações e FIIs da B3 para fila no SQS.
+
+    Esta função é responsável por utilizar as funcionalidades da biblioteca
+    pynvest (módulo fundamentus) para extrair toda a listagem de tickers
+    (códigos) de Ações e Fundos Imobiliários listados na B3 e, posteriormente,
+    enviar tais informações para uma fila pré configurada no SQS.
+
+    Args:
+        event (dict):
+            Evento de entrada da chamada da função. Nesse caso, como o gatilho
+            da função é dado como uma fila SQS, o dicionário de eventos contém
+            informações sobre mensagens enviadas para a fila.
+
+        context (LambdaContext): Metadados da própria função.
+
+    Return:
+        Dicionário contendo informações sobre o resultado de execução da função
+    """
+
+    # Instanciando objeto de scrapper do pynvest
+    pynvest_scrapper = Fundamentus(logger_level=logging.DEBUG)
+    pynvest_scrapper.logger.propagate = False
+
+    total_msgs = len(event["Records"])
+    logger.info("Quantidade de mensagens recebidas para processamento: "
+                f"{total_msgs}")
+
+    return 200
--- lambda.tf
+++ lambda.tf
@@ -34,4 +34,45 @@ resource "aws_lambda_function" "pynvest-lambda-send-b3-tickers-to-sqs-queue" {
   layers = [
     "arn:aws:lambda:${local.region_name}:336392948345:layer:AWSSDKPandas-Python310:5"
   ]
+
+  depends_on = [
+    data.archive_file.pynvest-lambda-send-b3-tickers-to-sqs-queue,
+    aws_iam_role.pynvest-lambda-send-msgs-to-queue
+  ]
+}
+
+
+/* -------------------------------------------------------
+    Lambda function
+    pynvest-lambda-get-financial-raw-data-to-s3
+------------------------------------------------------- */
+
+# Criando pacote zip da função a ser criada
+data "archive_file" "pynvest-lambda-get-financial-raw-data-to-s3" {
+  type        = "zip"
+  source_dir  = "${path.module}/app/lambda/functions/pynvest-lambda-get-financial-raw-data-to-s3/"
+  output_path = "${path.module}/app/lambda/zip/pynvest-lambda-get-financial-raw-data-to-s3.zip"
+}
+
+# Criando função Lambda
+resource "aws_lambda_function" "pynvest-lambda-get-financial-raw-data-to-s3" {
+  function_name = "pynvest-lambda-get-financial-raw-data-to-s3"
+  description   = "Processa mensagens de fila SQS, coleta indicadores financeiros de ativos e salva os dados no S3"
+
+  filename         = "${path.module}/app/lambda/zip/pynvest-lambda-get-financial-raw-data-to-s3.zip"
+  source_code_hash = data.archive_file.pynvest-lambda-get-financial-raw-data-to-s3.output_base64sha256
+
+  role    = aws_iam_role.pynvest-lambda-poll-msgs-from-queue-and-put-sor-data-to-s3.arn
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.10"
+  timeout = 180
+
+  layers = [
+    "arn:aws:lambda:${local.region_name}:336392948345:layer:AWSSDKPandas-Python310:5"
+  ]
+
+  depends_on = [
+    data.archive_file.pynvest-lambda-get-financial-raw-data-to-s3,
+    aws_iam_role.pynvest-lambda-poll-msgs-from-queue-and-put-sor-data-to-s3
+  ]
 }
