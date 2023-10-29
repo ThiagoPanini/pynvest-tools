a55ceb0bb44d3eced0ea93367c3c021e8665f158
Thiago Panini <panini.development@gmail.com>
2023-10-29 18:27:02 -0300

    feat: mvp da função Lambda para processar dados de FIIs

--- lambda.tf
+++ lambda.tf
@@ -84,7 +84,7 @@ resource "aws_lambda_function" "pynvest-lambda-get-financial-data-for-acoes" {
 }
 
 # Definindo gatilho para função: fila SQS
-resource "aws_lambda_event_source_mapping" "pynvest-tickers-queue" {
+resource "aws_lambda_event_source_mapping" "pynvest-tickers-queue-acoes" {
   function_name    = aws_lambda_function.pynvest-lambda-get-financial-data-for-acoes.arn
   event_source_arn = aws_sqs_queue.pynvest-tickers-acoes-queue.arn
 
@@ -102,3 +102,63 @@ resource "aws_lambda_event_source_mapping" "pynvest-tickers-queue" {
   ]
 }
 
+
+/* -------------------------------------------------------
+    Lambda function
+    pynvest-lambda-get-financial-data-for-fiis
+------------------------------------------------------- */
+
+# Criando pacote zip da função a ser criada
+data "archive_file" "pynvest-lambda-get-financial-data-for-fiis" {
+  type        = "zip"
+  source_dir  = "${path.module}/app/lambda/functions/pynvest-lambda-get-financial-data-for-fiis/"
+  output_path = "${path.module}/app/lambda/zip/pynvest-lambda-get-financial-data-for-fiis.zip"
+}
+
+# Criando função Lambda
+resource "aws_lambda_function" "pynvest-lambda-get-financial-data-for-fiis" {
+  function_name = "pynvest-lambda-get-financial-data-for-fiis"
+  description   = "Extrai e consolida indicadores financeiros de Ações a partir de tickers coletados de fila SQS"
+
+  filename         = "${path.module}/app/lambda/zip/pynvest-lambda-get-financial-data-for-fiis.zip"
+  source_code_hash = data.archive_file.pynvest-lambda-get-financial-data-for-fiis.output_base64sha256
+
+  role    = aws_iam_role.pynvest-lambda-put-sor-data-for-fiis.arn
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.10"
+  timeout = 180
+
+  layers = [
+    "arn:aws:lambda:${local.region_name}:336392948345:layer:AWSSDKPandas-Python310:5"
+  ]
+
+  environment {
+    variables = {
+      DATABASE_NAME = var.database_name
+    }
+  }
+
+  depends_on = [
+    data.archive_file.pynvest-lambda-get-financial-data-for-fiis,
+    aws_iam_role.pynvest-lambda-put-sor-data-for-fiis
+  ]
+}
+
+# Definindo gatilho para função: fila SQS
+resource "aws_lambda_event_source_mapping" "pynvest-tickers-queue-fiis" {
+  function_name    = aws_lambda_function.pynvest-lambda-get-financial-data-for-fiis.arn
+  event_source_arn = aws_sqs_queue.pynvest-tickers-fiis-queue.arn
+
+  # Configuração do trigger
+  batch_size                         = var.sqs_lambda_trigger_batch_size
+  maximum_batching_window_in_seconds = var.sqs_lambda_trigger_batch_window
+
+  scaling_config {
+    maximum_concurrency = var.sqs_lambda_trigger_max_concurrency
+  }
+
+  depends_on = [
+    aws_lambda_function.pynvest-lambda-get-financial-data-for-fiis,
+    aws_sqs_queue.pynvest-tickers-fiis-queue
+  ]
+}
