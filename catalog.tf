4c334485550e212ea6599eedc4166491d5d8de72
Thiago Panini <panini.development@gmail.com>
2023-10-31 15:04:12 -0300

    chore: estabelecendo criação de databases

--- catalog.tf
+++ catalog.tf
@@ -6,7 +6,15 @@ relacionados ao Glue Data Catalog para fins de catalogação
 de databases e tabelas geradas a partir deste projeto.
 -------------------------------------------------------- */
 
-resource "aws_glue_catalog_database" "fundamentus" {
-  count = var.flag_create_database ? 1 : 0
-  name  = var.database_name
+/*
+resource "aws_glue_catalog_database" "db_fundamentus_sor" {
+  count = var.flag_create_databases ? 1 : 0
+  name  = var.sor_database_name
+}
+*/
+
+resource "aws_glue_catalog_database" "databases_fundamentus" {
+  for_each    = var.flag_create_databases ? var.databases_names_map : {}
+  name        = each.value
+  description = "Armazenamento de dados ${upper(each.key)} do projeto pynvest"
 }
