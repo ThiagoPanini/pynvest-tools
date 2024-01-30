be30a4c6ec1341c4fb6e8d7a65c52f6572f35d0f
Thiago Panini <panini.development@gmail.com>
2024-01-29 23:29:22 -0300

    fix: ajustando paths temporários para leitura de schema em JSON

--- infra/modules/catalog/tbsor_fundamentus_indicadores_acoes.tf
+++ infra/modules/catalog/tbsor_fundamentus_indicadores_acoes.tf
@@ -10,7 +10,7 @@ pynvest aplicada ao site Fundamentus.
 
 # Criando tabela no Glue Data Catalog
 resource "aws_glue_catalog_table" "tbsor_fundamentus_indicadores_brutos_acoes" {
-  name          = local.tables_names_map["fundamentus"]["sor_acoes"]
+  name          = var.tables_names_map["fundamentus"]["sor_acoes"]
   database_name = var.databases_names_map["sor"]
   description   = "Tabela responsável por armazenar dados de indicadores de ações financeiras extraídos através de um motor de web scrapping apontado para o site Fundamentus"
 
@@ -22,12 +22,12 @@ resource "aws_glue_catalog_table" "tbsor_fundamentus_indicadores_brutos_acoes" {
   }
 
   storage_descriptor {
-    location      = "s3://${var.bucket_names_map["sor"]}/${local.tables_names_map["fundamentus"]["sor_acoes"]}"
+    location      = "s3://${var.bucket_names_map["sor"]}/${var.tables_names_map["fundamentus"]["sor_acoes"]}"
     input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
     output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
 
     ser_de_info {
-      name                  = "stream-${local.tables_names_map["fundamentus"]["sor_acoes"]}"
+      name                  = "stream-${var.tables_names_map["fundamentus"]["sor_acoes"]}"
       serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
 
       parameters = {
@@ -36,7 +36,7 @@ resource "aws_glue_catalog_table" "tbsor_fundamentus_indicadores_brutos_acoes" {
     }
 
     dynamic "columns" {
-      for_each = jsondecode(file("${path.root}/infra/modules/catalog/schemas/${local.tables_names_map["fundamentus"]["sor_acoes"]}.json"))["columns"]
+      for_each = jsondecode(file("${path.module}/schemas/${var.tables_names_map["fundamentus"]["sor_acoes"]}.json"))["columns"]
       content {
         name    = columns.value["name"]
         type    = columns.value["type"]
