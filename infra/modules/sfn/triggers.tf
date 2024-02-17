71df461b1e07266de8d09bf18d7d9a17d9a51624
Thiago Panini <panini.development@gmail.com>
2024-02-16 23:30:17 -0300

    feat: padronizando nomenclatura de recursos

--- infra/modules/sfn/triggers.tf
+++ infra/modules/sfn/triggers.tf
@@ -14,19 +14,20 @@ do Step Functions
 
 
 # Definindo regra de execução agendada via Eventbridge
-resource "aws_cloudwatch_event_rule" "trigger-sfn-dedup-sot-spec-tables" {
-  name                = "trigger-${aws_sfn_state_machine.sfn-dedup-sot-spec-tables.name}"
-  description         = "Regra de execução agendada do workflow Step Functions ${aws_sfn_state_machine.sfn-dedup-sot-spec-tables.name}"
+resource "aws_cloudwatch_event_rule" "trigger-pynvest-sfn-dedup-sot-spec-tables" {
+  name                = "trigger-${aws_sfn_state_machine.pynvest-sfn-dedup-sot-spec-tables.name}"
+  description         = "Regra de execução agendada do workflow Step Functions ${aws_sfn_state_machine.pynvest-sfn-dedup-sot-spec-tables.name}"
   schedule_expression = var.cron_expression_to_start_sfn_workflow
 }
 
 # Vinculando regra de agendamento à função
-resource "aws_cloudwatch_event_target" "trigger-sfn-dedup-sot-spec-tables" {
-  arn  = aws_sfn_state_machine.sfn-dedup-sot-spec-tables.arn
-  rule = aws_cloudwatch_event_rule.trigger-sfn-dedup-sot-spec-tables.name
+resource "aws_cloudwatch_event_target" "trigger-pynvest-sfn-dedup-sot-spec-tables" {
+  arn      = aws_sfn_state_machine.pynvest-sfn-dedup-sot-spec-tables.arn
+  rule     = aws_cloudwatch_event_rule.trigger-pynvest-sfn-dedup-sot-spec-tables.name
+  role_arn = var.iam_roles_arns_map["pynvest-sfn-invoke-lambda-functions"]
 
   depends_on = [
-    aws_sfn_state_machine.sfn-dedup-sot-spec-tables,
-    aws_cloudwatch_event_rule.trigger-sfn-dedup-sot-spec-tables
+    aws_sfn_state_machine.pynvest-sfn-dedup-sot-spec-tables,
+    aws_cloudwatch_event_rule.trigger-pynvest-sfn-dedup-sot-spec-tables
   ]
 }
