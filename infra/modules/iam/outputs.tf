1a0163d7b0a4c3d79764938003fe2810a49d10fb
Thiago Panini <panini.development@gmail.com>
2024-02-16 22:29:43 -0300

    feat: adicionando ARN da role do step functions como output do módulo IAM

--- infra/modules/iam/outputs.tf
+++ infra/modules/iam/outputs.tf
@@ -16,5 +16,6 @@ output "iam_roles_arns_map" {
     "pynvest-lambda-share-sot-financial-data"    = aws_iam_role.pynvest-lambda-share-sot-financial-data.arn,
     "pynvest-lambda-share-spec-financial-data"   = aws_iam_role.pynvest-lambda-share-spec-financial-data.arn,
     "pynvest-lambda-dedup-financial-data"        = aws_iam_role.pynvest-lambda-dedup-financial-data.arn
+    "pynvest-sfn-invoke-lambda-functions"        = aws_iam_role.pynvest-sfn-invoke-lambda-functions.arn
   }
 }
